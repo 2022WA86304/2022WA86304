0ac8c16f3483b7beb10c09c6b67057ce977ae9f9
2022WA86304 <moneyheist8443@gmail.com>
2026-09-25 11:51:39 +0530

    Second Build

---
+++ hello world.java
@@ -0,0 +1,15 @@
+package com.bits.devops;
+
+/**
+ * BITS ID : 2022WA86304, NAME: SAJID AHMED
+ */
+public class App {
+
+    public static String getMessage() {
+        return "DevOps Lab - Continuous Build using Maven - Version 2.0";
+    }
+
+    public static int addInt(int a, int b) {
+        return a + b;
+    }
+}
